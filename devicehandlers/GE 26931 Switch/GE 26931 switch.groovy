153ad0f8cecfc96dd8191068136144d2f1c9a65f
jrfarrar <register@jrfarrar.com>
2018-11-11 14:38:22 -0500

    version info

--- devicehandlers/GE 26931 Switch/GE 26931 switch.groovy
+++ devicehandlers/GE 26931 Switch/GE 26931 switch.groovy
@@ -5,6 +5,8 @@
  *
  * Copyright (C) Matt LeBaugh
  *
+ *  Version 1.1.0 11/11/18 - Modified for Hubitat - jrf
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
  *
