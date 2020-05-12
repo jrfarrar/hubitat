a7028bce67cb61d73c51fafea8acd6a9ca9d13a4
jrfarrar <register@jrfarrar.com>
2020-05-12 10:23:09 -0400

    Update AdminToolsParent.groovy

--- Apps/AdminTools/AdminToolsParent.groovy
+++ Apps/AdminTools/AdminToolsParent.groovy
@@ -17,7 +17,7 @@ definition(
     iconUrl: "",
     iconX2Url: "",
     iconX3Url: "",
-    importUrl: "",
+    importUrl: "https://raw.githubusercontent.com/jrfarrar/hubitat/master/Apps/AdminTools/AdminToolsParent.groovy",
 )
 
 preferences {
