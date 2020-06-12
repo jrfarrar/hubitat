8d91786636b8ebf952fe573ffb954b5bdb17453b
jrfarrar <register@jrfarrar.com>
2020-06-12 10:21:35 -0400

    Update garadgetmqtt.groovy

--- devicehandlers/garadgetMQTT/garadgetmqtt.groovy
+++ devicehandlers/garadgetMQTT/garadgetmqtt.groovy
@@ -9,6 +9,7 @@ metadata {
         capability "Switch"
         capability "Refresh"
         capability "Illuminance Measurement"
+        capability "Configuration"
         
         attribute "status", "string"
         attribute "time", "string"
@@ -18,17 +19,34 @@ metadata {
 
         //command "get-status", ["String"]
         //command "getstatus"
-}
-
-    preferences {
+preferences {
+        //paragraph "Press Configuration button after changing preferences."
+    section("Settings for connection from HE to Broker") {
         input (name: "doorName", type: "text", title: "Garadget Door Name(Topic name)")
-        input (name: "ipAddr", type: "text", title: "IP Address and port: ex: 192.168.0.1:1833")
+        input (name: "ipAddr", type: "text", title: "IP Address and port: ex: 192.168.0.1:1833 this is for this DH to connect to")
+        }
+    section("Settings for Garadget"){
         // put configuration here
+        input ( "rdt", "number", title: "sensor scan interval in mS (200-60,000, default 1,000)", defaultValue: 1000,range: "200..60000", required: false)
+        input ( "mtt", "number", title: "door moving time in mS from completely opened to completely closed (1,000 - 120,000, default 10,000)", defaultValue: 10000,range: "1000..120000", required: false)
+        input ( "rlt", "number", title: "button press time mS, time for relay to keep contacts closed (10-2,000, default 300)", defaultValue: 300,range: "10..2000", required: false)
+        input ( "rlp", "number", title: "delay between consecutive button presses in mS (10-5,000 default 1,000)", defaultValue: 1000,range: "10..5000", required: false)
+        input ( "srt", "number", title: "reflection threshold below which the door is considered open (1-80, default 25)", defaultValue: 25,range: "1..80", required: false)
+        //input ( "nme", "text", title: "device name to be used in MQTT topic. If cloud connection enabled, at reboot this value will be overwritten with the one saved in cloud via the apps", required: false)
+        input ( "mqtt", "text", title: "bitmap 0x01 - cloud enabled, 0x02 - mqtt enabled, 0x03 - cloud and mqtt enabled", defaultValue: "0x03", required: false)
+        input ( "mqip", "text", title: "MQTT broker IP address(IP for Garadget to connect to)", required: false)
+        input ( "mqpt", "number", title: "MQTT broker port number(port for Garadget to connect to)", required: false)
+        //input ( "mqus", "text", title: "MQTT user", required: false)
+        input ( "mqto", "number", title: "MQTT timeout (keep alive) in seconds", defaultValue: 15, required: false)
+        }
+    section("Logging"){
+        //logging
         input(name: "logLevel",title: "IDE logging level",multiple: false,required: true,type: "enum",options: getLogLevels(),submitOnChange : false,defaultValue : "1")
-        
-    }
-
+        }
+     }
+  }
 }
+
 
 void installed() {
     log.warn "installed..."
@@ -36,8 +54,8 @@ void installed() {
 
 // Parse incoming device messages to generate events
 void parse(String description) {
-    //topicFull=interfaces.mqtt.parseMessage(description).topic
-	//def topic=topicFull.split('/')
+    topicFull=interfaces.mqtt.parseMessage(description).topic
+	def topic=topicFull.split('/')
 	//def topicCount=topic.size()
 	//def payload=interfaces.mqtt.parseMessage(description).payload.split(',')
     //log.debug "Desc.payload: " + interfaces.mqtt.parseMessage(description).payload
@@ -45,39 +63,72 @@ void parse(String description) {
     //log.debug "json= " + json
     //log.debug "topic0: " + topic[0]
     //log.debug "topic1: " + topic[1]
-    //log.debug "topic2: " + topic[2]
+    log.debug "topic2: " + topic[2]
     //top=interfaces.mqtt.parseMessage(description).topic
 
     def message=interfaces.mqtt.parseMessage(description).payload
     jsonVal=parseJson(message)
-
+    if (topic[2] == "status") {
+        getStatus(jsonVal)
+    } else if (topic[2] == "config") {
+        getConfig(jsonVal)
+    }
     
-    debuglog "status: " + jsonVal.status
-    debuglog "bright: " + jsonVal.bright
-    debuglog "sensor: " + jsonVal.sensor
-    debuglog "signal: " + jsonVal.signal
-    debuglog "time: " + jsonVal.time
-    if (jsonVal.status == "closed") {
+
+}
+
+void getStatus(status) {
+    debuglog "status: " + status.status
+    debuglog "bright: " + status.bright
+    debuglog "sensor: " + status.sensor
+    debuglog "signal: " + status.signal
+    debuglog "time: " + status.time
+    if (status.status == "closed") {
         sendEvent(name: "contact", value: "closed")
         sendEvent(name: "door", value: "closed")
-    } else if (jsonVal.status == "open") {
+    } else if (status.status == "open") {
         sendEvent(name: "contact", value: "open")
         sendEvent(name: "door", value: "open")
-    } else if (jsonVal.status == "stopped") {
+    } else if (status.status == "stopped") {
         sendEvent(name: "door", value: "stopped")
         sendEvent(name: "door", value: "open")
-    } else if (jsonVal.status == "opening") {
+    } else if (status.status == "opening") {
         sendEvent(name: "door", value: "opening")
-    } else if (jsonVal.status == "closing") {
+    } else if (status.status == "closing") {
         sendEvent(name: "door", value: "closing")
     } else {
         infolog "unknown event"
     }
-    sendEvent(name: "sensor", value: jsonVal.sensor)
-    sendEvent(name: "signal", value: jsonVal.signal)
-    sendEvent(name: "bright", value: jsonVal.bright)
-    sendEvent(name: "time", value: jsonVal.time)
-    sendEvent(name:"illuminance", value: jsonVal.bright)
+    sendEvent(name: "sensor", value: status.sensor)
+    sendEvent(name: "signal", value: status.signal)
+    sendEvent(name: "bright", value: status.bright)
+    sendEvent(name: "time", value: status.time)
+    sendEvent(name: "illuminance", value: status.bright)
+}
+void getConfig(config) {
+    debuglog "rdt: " + config.rdt + " - sensor scan interval in mS (200-60,000, default 1,000)"
+    debuglog "mtt: " + config.mtt + " - door moving time in mS from completely opened to completely closed (1,000 - 120,000, default 10,000)"
+    debuglog "rlt: " + config.rlt + " - button press time mS, time for relay to keep contacts closed (10-2,000, default 300)"
+    debuglog "rlp: " + config.rlp + " - delay between consecutive button presses in mS (10-5,000 default 1,000)"
+    debuglog "srt: " + config.srt + " - reflection threshold below which the door is considered open (1-80, default 25)"
+    debuglog "nme: " + config.nme + " - device name to be used in MQTT topic. If cloud connection enabled, at reboot this value will be overwritten with the one saved in cloud via the apps"
+    debuglog "mqtt: " + config.mqtt + " - bitmap 0x01 - cloud enabled, 0x02 - mqtt enabled, 0x03 - cloud and mqtt enabled"
+    debuglog "mqip: " + config.mqip + " - MQTT broker IP address"
+    debuglog "mqpt: " + config.mqpt + " - MQTT broker port number"
+    debuglog "mqus: " + config.mqus + " - MQTT user"
+    debuglog "mqto: " + config.mqto + " - MQTT timeout (keep alive) in seconds"
+    rdt = config.rdt
+    mtt = config.mtt
+    rlt = config.rlt
+    rlp = config.rlp
+    srt = config.srt
+    nme = config.nme
+    mqtt = config.mqtt
+    mqip = config.mqip
+    mqpt = config.mqpt
+    mqus = config.mqus
+    mqto = config.mqto
+
 }
 
 //void publishMsg(String s) {
@@ -98,6 +149,7 @@ void refresh(){
 void getstatus() {
     debuglog "getstatus command sent"
     interfaces.mqtt.publish("garadget/${doorName}/command", "get-status")
+    interfaces.mqtt.publish("garadget/${doorName}/command", "get-config")
 }
 void updated() {
     infolog "updated..."
@@ -117,19 +169,31 @@ void initialize() {
         pauseExecution(1000)
         infolog "connection established"
         mqttInt.subscribe("garadget/${doorName}/status")
+        mqttInt.subscribe("garadget/${doorName}/config")
         //mqttInt.subscribe("test/hubitat")
     } catch(e) {
         debuglog "initialize error: ${e.message}"
     }
+    configure()
 }
+
+void configure(){
+    infolog "Configure..."
+    if (rdt) {
+        debuglog "Configure rdt"
+        
+    }
+    //interfaces.mqtt.publish("garadget/${doorName}/set-config", json)
+}
+
 void mqttClientStatus(String message) {
 	infolog "Received status message ${message}"
 }
-def on() {
+void on() {
     debuglog "Sending on event to open door"
 	open()
 }
-def off() {
+void off() {
     debuglog "Sending off event to close door"  
 	close()
 }
