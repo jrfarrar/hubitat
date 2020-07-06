f2b8aa0b84ed271bc0282242c11793c6eb8affb9
jrfarrar <register@jrfarrar.com>
2020-07-06 10:30:04 -0400

    Update SepticPump.groovy

--- Apps/AdminTools/SepticPump.groovy
+++ Apps/AdminTools/SepticPump.groovy
@@ -239,7 +239,7 @@ def debuglog(statement)
     if (logL == 0) {return}//bail
     else if (logL >= 2)
 	{
-		log.debug(statement)
+		log.debug("$thisName: " + statement)
 	}
 }
 def infolog(statement)
@@ -249,7 +249,7 @@ def infolog(statement)
     if (logL == 0) {return}//bail
     else if (logL >= 1)
 	{
-		log.info(statement)
+		log.info("$thisName: " + statement)
 	}
 }
 def getLogLevels(){
